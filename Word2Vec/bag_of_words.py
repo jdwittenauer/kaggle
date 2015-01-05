4acaec6513290ffc6538e94d6db2710afd991317
John Wittenauer <jdwittenauer@gmail.com>
2015-01-05 16:56:15 -0500

    Modified files to conform to PEP8 naming conventions.

--- Word2Vec/bag_of_words.py
+++ Word2Vec/bag_of_words.py
@@ -15,7 +15,7 @@ sys.path.append('C:\\Users\\John\\PycharmProjects\\Kaggle\\Word2Vec')
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from KaggleUtility import KaggleUtility
+from kaggle_utility import KaggleUtility
 
 
 def main():
